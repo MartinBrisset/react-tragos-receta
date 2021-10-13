86db3631d8dc25c102f4580c2af7d476f9249c65
Martin Brisset <brissetmartin@gmail.com>
2021-10-13 14:29:05 -0300

    add docs

---
+++ docs/precache-manifest.a64b66849458fa9a5bbd3d6d48a6f2fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92ac0cb18d9237b6074c053a84b4cd42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d878743179a3b281ef56",
+    "url": "/static/css/main.da14feb7.chunk.css"
+  },
+  {
+    "revision": "d8d1abcb8c0809e5f2c9",
+    "url": "/static/js/2.ba6d0e25.chunk.js"
+  },
+  {
+    "revision": "8f75fe0532614f1d4d17c26594b021fc",
+    "url": "/static/js/2.ba6d0e25.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d878743179a3b281ef56",
+    "url": "/static/js/main.91df9bd4.chunk.js"
+  },
+  {
+    "revision": "11e42c8855003a14000a",
+    "url": "/static/js/runtime-main.d2c02b9f.js"
+  }
+]);
